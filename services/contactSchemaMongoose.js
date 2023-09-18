32fb0379933133062d3fcb3a30dbea492501588d
Jakub Piksa <kuba.piksa@wp.pl>
2023-09-18 21:37:58 +0200

    bit of progress

--- services/contactSchemaMongoose.js
+++ services/contactSchemaMongoose.js
@@ -1,15 +1,18 @@
 const Joi = require('joi');
 const Contact = require('../models/contacts');
 
-const validateContact = (contact) => {
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required(),
-  });
+// const validateContact = (contact) => {
+//   const schema = Joi.object({
+//     name: Joi.string().required(),
+//     email: Joi.string().email().required(),
+//     phone: Joi.string().required(),
+//     favorite: Joi.boolean().invalid(false).required(),
+//   });
 
-  return schema.validate(contact);
-};
+//   return schema.validate(contact);
+// };
+
+
 const listContacts = async () => {
   try {
     return await Contact.find()
@@ -28,27 +31,26 @@ const removeContact = async (contactId) => {
 };
 
 const addContact = async (name, email, phone, favorite) => {
-  const newContact = new Contact({
-    name,
-    email,
-    phone,
-    favorite
-  });
+  try {
+    const newContact = new Contact({
+      name,
+      email,
+      phone,
+      favorite
+    });
 
-  const validation = validateContact(newContact);
-  if (validation.error) {
-    throw new Error(validation.error.details[0].message);
+    return await newContact.save();
+  } catch (error) {
+    throw new Error('Could not create contact: ' + error.message);
   }
-
-  return await newContact.save();
 };
+
 
 const updateContact = async (contactId, body) => {
   return await Contact.findByIdAndUpdate(contactId, body, { new: true });
 };
 
 module.exports = {
-  validateContact,
   listContacts,
   getContactById,
   removeContact,
