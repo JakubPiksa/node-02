8475916fefc86a4c38a2f3b2660f77d174f87947
Jakub Piksa <kuba.piksa@wp.pl>
2023-09-21 20:21:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ mongoose.connect(dbUrl, {
 });
 const db = mongoose.connection;
 
-
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
+
   console.log('Connected to MongoDB');
 });
 
